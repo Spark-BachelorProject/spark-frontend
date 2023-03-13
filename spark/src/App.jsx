cacbedb28fd608c2d772f723e85c394b083ecefc
Dawid Grabek <grabekd37@gmail.com>
2023-03-13 18:50:55 +0100

    Add and configure styled components

--- spark/src/App.jsx
+++ spark/src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './assets/styles/theme'
+import { GlobalStyle } from './assets/styles/GlobalStyle'
+import styled from 'styled-components'
+
+const MainDiv = styled.div`
+  // its only example of component
+  background-color: blue;
+  width: 100vw;
+  height: 300px;
+`
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <MainDiv>hello there</MainDiv>
+    </ThemeProvider>
   )
 }
 

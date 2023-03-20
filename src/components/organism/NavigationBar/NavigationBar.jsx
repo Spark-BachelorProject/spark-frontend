e6636bd0aa7c838232fe948c5f16995e420846f8
Dawid Grabek <grabekd37@gmail.com>
2023-03-20 21:22:17 +0100

    Rename NavigationBar file

--- src/components/organism/NavigationBar/NavigationBar.jsx
+++ src/components/organism/NavigationBar/NavigationBar.jsx
@@ -4,7 +4,11 @@ import { ReactComponent as HomeIcon } from '@/assets/icons/home.svg'
 import { ReactComponent as UsersIcon } from '@/assets/icons/users.svg'
 import { ReactComponent as MapIcon } from '@/assets/icons/map.svg'
 import { ReactComponent as UerIcon } from '@/assets/icons/user.svg'
-import { Wrapper, IconsWrapper, StyledPlusSquareIcon } from './NavigationBar.js'
+import {
+  Wrapper,
+  IconsWrapper,
+  StyledPlusSquareIcon,
+} from './NavigationBar.styles'
 
 const NavigationBar = () => {
   return (

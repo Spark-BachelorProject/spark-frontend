3f07b68416ac5c3deb8fcc31343b1948e80727c2
Karol Gralewski <gralewski.karol@gmail.com>
2023-03-25 19:17:30 +0100

    Add header text

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -5,6 +5,7 @@ export const lightTheme = {
   iconPrimary: '#94a3b8', //slate400
   iconSecondary: '#475569', //slate600
   inputFont: '#475569', //slate600
+  textHeader: '#475569', //slate600
   white: '#fff', //white
   secondaryBg: '#fff', //white
   iconBg: '#fff', //white
@@ -15,6 +16,7 @@ export const lightTheme = {
 export const darkTheme = {
   iconPrimaryActive: '#f8fafc', //slate50
   inputFont: '#e2e8f0', //slate200
+  textHeader: '#e2e8f0', //slate200
   iconPrimary: '#64748B', //slate500
   divider: '#64748B', //slate500
   iconBg: '#233045', //slate750

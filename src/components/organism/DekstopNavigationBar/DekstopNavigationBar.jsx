c1e34bd0da4405aac0031780496d424fecfaa9d2
Dawid Grabek <grabekd37@gmail.com>
2023-04-17 12:49:13 +0200

    More separate to more files comonent DesktopNavigationBar

--- src/components/organism/DekstopNavigationBar/DekstopNavigationBar.jsx
+++ src/components/organism/DekstopNavigationBar/DekstopNavigationBar.jsx
@@ -1,28 +1,12 @@
 import React from 'react'
-import { NavigationSection, Wrapper } from './DesktopNavigationBar.styles'
-import { NavLink } from 'react-router-dom'
-import { ReactComponent as HomeIcon } from '@/assets/icons/home.svg'
-import { ReactComponent as UsersIcon } from '@/assets/icons/users.svg'
-import { ReactComponent as MapIcon } from '@/assets/icons/map.svg'
+import { Wrapper } from './DesktopNavigationBar.styles'
 import SocialsSection from '../SocialsSection/SocialsSection'
+import NavigationSection from '../NavigationSection/NavigationSection'
 
 export const DekstopNavigationBar = () => {
   return (
     <Wrapper>
-      <NavigationSection>
-        <NavLink to="/">
-          <HomeIcon />
-          Wszystkie
-        </NavLink>
-        <NavLink to="/users">
-          <UsersIcon />
-          Społeczności
-        </NavLink>
-        <NavLink to="/map">
-          <MapIcon />
-          Mapa Aktywności
-        </NavLink>
-      </NavigationSection>
+      <NavigationSection />
       <SocialsSection />
     </Wrapper>
   )
---
+++ src/components/organism/NavigationSection/NavigationSection.styles.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.nav`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  position: relative;
+  margin-bottom: 60px;
+
+  & > a {
+    font-size: ${({ theme }) => theme.fontSize.l};
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 15px;
+    color: ${({ theme }) => theme.colors.navigationInactive};
+    text-decoration: none;
+    font-weight: 400;
+    padding: 10px 15px;
+    border-radius: 7px;
+  }
+  // TODO: check colors
+  & > a.${'active'} {
+    color: ${({ theme }) => theme.colors.textHeader};
+    font-weight: 600;
+  }
+
+  & > a:hover {
+    background-color: ${({ theme }) => theme.colors.modalBg};
+    transition: 200ms ease-in-out;
+  }
+
+  & > a > svg > path {
+    stroke: ${({ theme }) => theme.colors.navigationInactive};
+  }
+
+  & > a.${'active'} > svg > path {
+    stroke: ${({ theme }) => theme.colors.iconPrimaryActive};
+  }
+
+  &::before {
+    position: absolute;
+    content: '';
+    bottom: -30px;
+    left: 0;
+    width: 100%;
+    height: 1px;
+    background-color: ${({ theme }) => theme.colors.divider};
+  }
+`

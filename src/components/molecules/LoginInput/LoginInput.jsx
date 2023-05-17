583dbdf0c053940efa286747b32ab519eb33df39
Dawid Grabek <grabekd37@gmail.com>
2023-05-17 11:52:59 +0200

    Add prop types

--- src/components/molecules/LoginInput/LoginInput.jsx
+++ src/components/molecules/LoginInput/LoginInput.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import PropTypes from 'prop-types'
+
 import { ReactComponent as AlertCircle } from '@/assets/icons/alert-circle.svg'
 import { Error } from '@/components/atoms/Error/Error.styles'
 import { Input } from '@/components/atoms/Input/Input.styles'
@@ -18,5 +20,14 @@ const LoginInput = React.forwardRef((props, ref) => {
     </>
   )
 })
+
+LoginInput.propTypes = {
+  error: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  onBlur: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  type: PropTypes.string.isRequired,
+}
 
 export default LoginInput

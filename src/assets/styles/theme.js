b640d493b6b2584b7d9d466636ecc634c9a7394e
Karol Gralewski <gralewski.karol@gmail.com>
2023-10-12 11:13:05 +0200

    refactor(theme): sort the variables

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -1,126 +1,118 @@
 export const lightTheme = {
-  navbarBg: '#fff', //white
-  navbarBorder: '#E2E8F0', //slate200
-  primaryBg: '#f8fafc', //slate50
+  accent: '#3b82f6', //blue500
   addPostBg: '#f8fafc', //slate50
-  selectBorder: '#f1f5f9', //slate100
-  buttonBorder: '#E2E8F0', //slate200
-  divider: '#E2E8F0', //slate200
-  darkDivider: '#E2E8F0', //slate200
-  iconPrimary: '#94a3b8', //slate400
-  iconSecondary: '#475569', //slate600
-  selectFont: '#475569', //slate600
-  inputFont: '#475569', //slate600
-  commentDetails: '#475569', //slate600
-  textHeader: '#334155', //slate700
-  white: '#fff', //white
-  black: '#000', //black
-  secondaryBg: '#fff', //white
-  iconBg: '#fff', //white
-  iconPlusBg: '#3b82f6', //blue500
-  buttonBg: '#3B82F6', //blue500
-  iconPrimaryActive: '#0f172a',
-  text: '#475569', //slate600
-  boldText: '#1e293b', //slate800
-  postBorder: '#f1f5f9', //slate100
-  mainDivider: '#f8fafc', //slate50
-  placeholder: '#94a3b8', //slate400
   attendingCounterBg: '#f8fafc', //slate50
+  black: '#000', //black
+  bookmarkBg: '#FAFBFF', //custom
+  boldText: '#1e293b', //slate800
+  buttonBg: '#3B82F6', //blue500
+  buttonBorder: '#E2E8F0', //slate200
   buttonFont: '#EFF6FF', //blue50
-  titleFont: '#0F172A', //slate900
-  tagFont: '#475569', //slate600
-  detailsFont: '#233045', //slate750
-  labelFont: '#1e293b', // slate800
-  modalBorder: '#f8fafc', //slate50
-  modalBg: '#fff', //white
-  inputBg: '#fff', //white
+  buttonOffColor: '#60a5fa', //blue400
+  buttonOffFont: '#f1f5f9', //slate100
   checkboxBg: '#fff', //white
   checkboxBorder: '#e2e8f0', //slate200
   checkboxTick: '#64748B', //slate500
-  accent: '#3b82f6', //blue500
+  commentDetails: '#475569', //slate600
+  darkDivider: '#E2E8F0', //slate200
+  detailsFont: '#233045', //slate750
+  divider: '#E2E8F0', //slate200
+  iconBg: '#fff', //white
+  iconPlusBg: '#3b82f6', //blue500
+  iconPrimary: '#94a3b8', //slate400
+  iconPrimaryActive: '#0f172a',
+  iconSecondary: '#475569', //slate600
+  inputBg: '#fff', //white
+  inputFont: '#475569', //slate600
+  labelFont: '#1e293b', // slate800
+  mainDivider: '#f8fafc', //slate50
+  modalBg: '#fff', //white
+  modalBorder: '#f8fafc', //slate50
+  navbarBg: '#fff', //white
+  navbarBorder: '#E2E8F0', //slate200
+  placeholder: '#94a3b8', //slate400
+  postBorder: '#f1f5f9', //slate100
+  primaryBg: '#f8fafc', //slate50
   redFont: '#D71E3B', //red500
-  bookmarkBg: '#FAFBFF', //custom
-  popupTriangle: '#fff', //white
-  counterBorder: '#fff', //white
-  buttonOffColor: '#60a5fa', //blue400
-  buttonOffFont: '#f1f5f9', //slate100
-  navigationInactive: '#64748B', //slate500
-  badgeHeader: '#2563EB', //blue600
-  textSecondary: '#CBD5E1', //slate300
+  secondaryBg: '#fff', //white
+  selectBorder: '#f1f5f9', //slate100
+  selectFont: '#475569', //slate600
   tagBg: '#3b82f609', //blue500
   tagBgActive: '#3b82f615', //blue500
   tagBorder: '#3b82f6', //blue500
+  tagFont: '#475569', //slate600
+  text: '#475569', //slate600
+  textHeader: '#334155', //slate700
+  textSecondary: '#CBD5E1', //slate300
+  titleFont: '#0F172A', //slate900
+  white: '#fff', //white
 }
 
 export const darkTheme = {
-  navbarBg: '#233045', //slate750
-  navbarBorder: '#334155', //slate700
-  iconPrimaryActive: '#f8fafc', //slate50
-  inputFont: '#e2e8f0', //slate200
-  commentDetails: '#e2e8f0', //slate200
-  textHeader: '#E2E8F0', //slate200
-  selectFont: '#CBD5E1', //slate300
-  iconPrimary: '#64748B', //slate500
-  divider: '#233045', //slate750
-  darkDivider: '#172233',
-  iconBg: '#233045', //slate750
-  selectBorder: '#233045', //slate750
-  buttonBorder: '#233045', //slate750
-  secondaryBg: '#1e293b', //slate800
-  modalBg: '#1e293b', //slate800
-  primaryBg: '#0f172a', //slate900
-  iconPlusBg: '#3b82f6', //blue500
-  buttonBg: '#3B76F5', //blue550
-  white: '#fff', //white
-  black: '#000', //black
-  labelFont: '#CBD5E1', // slate300
-  iconSecondary: '#e2e8f0',
-  text: '#CBD5E1', //slate300
-  boldText: '#CBD5E1', //slate300
-  postBorder: '#233045', //slate750
-  mainDivider: '#233045', //slate750
-  placeholder: '#94a3b8', //slate400
+  accent: '#3b82f6', //blue500
+  addPostBg: '#233045', //slate750
   attendingCounterBg: '#233045', //slate750
+  black: '#000', //black
+  bookmarkBg: '#233045', //slate750
+  boldText: '#CBD5E1', //slate300
+  buttonBg: '#3B76F5', //blue550
+  buttonBorder: '#233045', //slate750
   buttonFont: '#EFF6FF', //blue50
-  titleFont: '#F1F5F9', //slate100
-  tagFont: '#CBD5E1', //slate300
-  detailsFont: '#E2E8F0', //slate200
-  modalBorder: '#233045', //slate100
-  inputBg: '#233045', //slate750
+  buttonOffColor: '#3b82f6', //blue500
+  buttonOffFont: '#f1f5f9', //slate100
   checkboxBg: '#233045', //slate750
   checkboxBorder: '#334155', //slate700
   checkboxTick: '#CBD5E1', //slate300
-  accent: '#3b82f6', //blue500
+  commentDetails: '#e2e8f0', //slate200
+  darkDivider: '#172233',
+  detailsFont: '#E2E8F0', //slate200
+  divider: '#233045', //slate750
+  iconBg: '#233045', //slate750
+  iconPlusBg: '#3b82f6', //blue500
+  iconPrimary: '#64748B', //slate500
+  iconPrimaryActive: '#f8fafc', //slate50
+  iconSecondary: '#e2e8f0',
+  inputBg: '#233045', //slate750
+  inputFont: '#e2e8f0', //slate200
+  labelFont: '#CBD5E1', // slate300
+  mainDivider: '#233045', //slate750
+  modalBg: '#1e293b', //slate800
+  modalBorder: '#233045', //slate100
+  navbarBg: '#233045', //slate750
+  navbarBorder: '#334155', //slate700
+  placeholder: '#94a3b8', //slate400
+  postBorder: '#233045', //slate750
+  primaryBg: '#0f172a', //slate900
   redFont: '#D71E3B', //custom red
-  bookmarkBg: '#233045', //slate750
-  popupTriangle: '#233045', //slate100
-  counterBorder: '#233045', //slate750
-  buttonOffColor: '#3b82f6', //blue500
-  buttonOffFont: '#f1f5f9', //slate100
-  navigationInactive: '#CBD5E1', //slate300
-  badgeHeader: '#2563EB', //blue600
+  secondaryBg: '#1e293b', //slate800
+  selectBorder: '#233045', //slate750
+  selectFont: '#CBD5E1', //slate300
+  tagBg: '#3b82f610', //custom blue
+  tagBgActive: '#3b82f620', //custom blue
+  tagBorder: '#3b82f650', //custom blue
+  tagFont: '#CBD5E1', //slate300
+  text: '#CBD5E1', //slate300
+  textHeader: '#E2E8F0', //slate200
   textSecondary: '#64748B', //slate500
-  tagBg: '#3b82f610',
-  tagBgActive: '#3b82f620',
-  tagBorder: '#3b82f650',
-  addPostBg: '#233045', //slate750
+  titleFont: '#F1F5F9', //slate100
+  white: '#fff', //white
 }
 
 export const theme = {
-  colors: lightTheme,
-  fontSize: {
-    xxl: '34px',
-    xl: '24px',
-    l: '18px',
-    mPlus: '16px',
-    m: '15px',
-    sPlus: '14px',
-    s: '13px',
-    xs: '11px',
-  },
   breakPoints: {
     l: '1300px',
     m: '768px',
     mobile: '476px',
+  },
+  colors: lightTheme,
+  fontSize: {
+    xs: '11px',
+    s: '13px',
+    sPlus: '14px',
+    m: '15px',
+    mPlus: '16px',
+    l: '18px',
+    xl: '24px',
+    xxl: '34px',
   },
 }

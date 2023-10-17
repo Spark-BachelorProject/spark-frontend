dcec0221cdcf426dcd78a69f71732840c49f59f5
Karol Gralewski <gralewski.karol@gmail.com>
2023-10-17 15:03:58 +0200

    fix(groups): fix header image rendering

--- src/components/molecules/GroupsHeader/GroupsHeader.styles.js
+++ src/components/molecules/GroupsHeader/GroupsHeader.styles.js
@@ -3,11 +3,16 @@ import styled from 'styled-components'
 import { Text } from '@/components/atoms/Text/Text.styles'
 import { Title } from '@/components/atoms/Title/Title.styles'
 
+const image = Object.values(
+  import.meta.glob('@assets/images/banner.png', { eager: true, as: 'url' })
+)
+
+console.log(image[0])
+
 export const Wrapper = styled.div`
   width: 100%;
   margin-bottom: 20px;
-  //TODO: Add some background image
-  background-color: #221402;
+  background-image: url(${image[0]});
   border-radius: 5px;
   padding: 30px;
   display: flex;

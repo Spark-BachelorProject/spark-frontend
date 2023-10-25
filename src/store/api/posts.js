254823ee6032311c02c4f64d7f4c6a8228438825
adrian <agrski06@gmail.com>
2023-10-25 11:09:28 +0200

    feat: add authorization header WIP

--- src/store/api/posts.js
+++ src/store/api/posts.js
@@ -4,6 +4,7 @@ export const postsApi = createApi({
   reducerPath: 'postsApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'http://localhost:8080/api/v1',
+    headers: {Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwiaWF0IjoxNjk4MjI0NjA3LCJleHAiOjE2OTgyMjYwNDd9.wo3olsEUP6CYwe25ZdeOfvYuk4LGDM9x8bno5o3LSBc`}
   }),
   endpoints: (builder) => ({
     getPosts: builder.query({

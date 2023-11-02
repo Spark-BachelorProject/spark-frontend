aff3e19110d189f7a91312e3e6aeb4812e74f03d
Dawid Grabek <grabekd37@gmail.com>
2023-11-02 15:03:10 +0100

    feat(loader): add loader

--- src/components/pages/Home/Home.jsx
+++ src/components/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
+import Loader from '@/components/atoms/Loader/Loader'
 import { AddPostSection } from '@/components/molecules/AddPostSection/AddPostSection'
 import { TitleBar } from '@/components/molecules/TitleBar/TitleBar'
 import { Dropdown } from '@/components/organism/Dropdown/Dropdown'
@@ -20,6 +21,7 @@ const Home = () => {
         To się dzieje w <strong>Lublinie</strong>!
       </TitleBar>
       <Dropdown />
+      {isLoading && <Loader isCentered />}
       {!isLoading && posts.map((post) => <Post {...post} key={post.id} />)}
     </PageContent>
   )

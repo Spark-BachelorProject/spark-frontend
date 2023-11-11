bcb16bd95f2e1077e278821bd1159cd01a7de765
Dawid Grabek <grabekd37@gmail.com>
2023-11-11 20:00:27 +0100

    feat(profile): distinguish posts to active and archived

--- src/components/pages/Profile/Profile.jsx
+++ src/components/pages/Profile/Profile.jsx
@@ -1,25 +1,35 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 
 import { DividerLabel } from '@/components/atoms/DividerLabel/DividerLabel.styles'
+import Loader from '@/components/atoms/Loader/Loader'
+import { Title } from '@/components/atoms/Title/Title.styles'
 import Post from '@/components/organism/Post/Post'
 import ProfileDetails from '@/components/organism/ProfileDetails/ProfileDetails'
+import { getCurrentTimeISOString } from '@/helpers/dateAndTime'
 import { useGetPostsQuery } from '@/store/api/posts'
 
 import { StyledPageContent } from './Profile.styles'
 
-//TODO: posts should wider, i guess 684px
+//TODO: ask about some endpoint for active posts
 const Profile = () => {
-  // const posts = useSelector((state) => state.posts)
   const { data: posts, isLoading } = useGetPostsQuery()
+  const currentTime = getCurrentTimeISOString()
+
+  if (isLoading) {
+    return <Loader isCentered />
+  }
+  const activePosts = posts.filter((post) => currentTime < post.dateEnd)
+  const archivedPosts = posts.filter((post) => currentTime > post.dateEnd)
 
   return (
     <StyledPageContent>
       <ProfileDetails />
       <div>
         <DividerLabel>Aktualne posty</DividerLabel>
-        {!isLoading && posts.map((post, i) => <Post {...post} key={post.id} />)}
+        {!isLoading && activePosts.map((post) => <Post {...post} key={post.id} />)}
+        {!isLoading && activePosts.length === 0 && <Title>Brak aktywnych postów</Title>}
         <DividerLabel>Archiwalne posty</DividerLabel>
+        {!isLoading && archivedPosts.map((post) => <Post {...post} key={post.id} />)}
       </div>
     </StyledPageContent>
   )

23c766675b7da718a53c6ef67d742aba77f4498a
Karol Gralewski <gralewski.karol@gmail.com>
2023-12-07 13:16:56 +0100

    fix(app): fix redirect on f5

--- src/views/AuthenticatedApp.jsx
+++ src/views/AuthenticatedApp.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Navigate, Route, Routes } from 'react-router'
 
 import HeaderSearchBar from '@/components/organism/HeaderSearchBar/HeaderSearchBar'
--- src/views/UnauthenticatedApp.jsx
+++ src/views/UnauthenticatedApp.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Navigate, Route, Routes } from 'react-router'
 
 import UnauthenticatedFooter from '@/components/molecules/UnauthenticatedFooter/UnauthenticatedFooter'

b6da09e4a2f1004ce07de1ed649b72b3d4a92435
Karol Gralewski <gralewski.karol@gmail.com>
2023-12-09 15:14:41 +0100

    feat(map): add coming back to the previous position when the popup is closed

--- src/components/pages/Map/Map.jsx
+++ src/components/pages/Map/Map.jsx
@@ -14,11 +14,12 @@ import { Filters, OverlayRight, Wrapper } from './Map.styles'
 import './Map.styles.css'
 import { getIcon } from './customIcons'
 
-const UpdateCenter = ({ center, zoom }) => {
+const UpdateCenter = ({ center, zoom, setPrevCenter }) => {
   const map = useMap()
   useEffect(() => {
     map.flyTo(center, zoom)
-  }, [center, zoom, map])
+    setPrevCenter(map.getCenter())
+  }, [center, zoom, map, setPrevCenter])
   return null
 }
 
@@ -33,7 +34,7 @@ const firstData = {
   name: 'Wszystkie',
   id: 0,
 }
-//TODO: select one marker when there are no results
+
 export const Map = () => {
   const { data: posts, isLoading: isLoadingPosts, isSuccess: isSuccessPosts } = useGetPostsQuery()
   const {
@@ -43,16 +44,19 @@ export const Map = () => {
   } = useGetActivitiesQuery()
   const [activities, setActivities] = useState([])
   const [center, setCenter] = useState([LublinCoordinates.lat, LublinCoordinates.lng])
+  const [userLocation, setUserLocation] = useState(null) // New state for user's location
   const [selectedMarker, setSelectedMarker] = useState(null)
   const [activitySelect, setActivitySelect] = useState(null)
   const [allowedGeoLocation, setAllowedGeoLocation] = useState(false)
   const [zoom, setZoom] = useState(13)
+  const [prevCenter, setPrevCenter] = useState([null, null])
 
   const onActivityChange = (e) => {
     setActivitySelect(e.target.value)
   }
 
   const onMarkerClick = (marker) => {
+    setPrevCenter(center)
     setSelectedMarker(marker)
     setCenter([marker.location.latitude, marker.location.longitude])
     setZoom(16.5)
@@ -60,6 +64,7 @@ export const Map = () => {
 
   const onMapClick = () => {
     setSelectedMarker(null)
+    setCenter(prevCenter)
     setZoom(13)
   }
 
@@ -79,7 +84,7 @@ export const Map = () => {
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       function (position) {
-        setCenter([position.coords.latitude, position.coords.longitude])
+        setUserLocation([position.coords.latitude, position.coords.longitude]) // Set user's location
         setAllowedGeoLocation(true)
       },
       function (error) {
@@ -104,7 +109,6 @@ export const Map = () => {
           url="https://{s}.tile.jawg.io/jawg-streets/{z}/{x}/{y}{r}.png?access-token=aSetRCOQl0G3zH75uVIo4ZLmnMUgiP4uy5ss8IrkciB6DUwX8HUzf3he3SBU7Ppi"
           attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         />
-
         <MarkerClusterGroup
           chunkedLoading
           showCoverageOnHover={false}
@@ -129,8 +133,10 @@ export const Map = () => {
                 />
               ))}
         </MarkerClusterGroup>
-        {allowedGeoLocation && <Marker position={center} icon={getIcon('user')} />}
-        <UpdateCenter center={center} zoom={zoom} />
+        {allowedGeoLocation && userLocation && (
+          <Marker position={userLocation} icon={getIcon('user')} />
+        )}{' '}
+        <UpdateCenter center={center} zoom={zoom} setPrevCenter={setPrevCenter} />
       </MapContainer>
 
       <OverlayRight>
@@ -147,26 +153,6 @@ export const Map = () => {
           )}
         </Filters>
       </OverlayRight>
-      {/* <MapLegend>
-        {!isLoadingActivities &&
-          isSuccessActivities &&
-          activities.map((activity, index) =>
-            activity.name !== 'Wszystkie' ? (
-              <MapLegendItem key={index}>
-                <img src={getIconUrl(activity.name)} height={30} alt={`${activity.name} Marker`} />
-                <Text isBold key={index}>
-                  {activity.name}
-                </Text>
-              </MapLegendItem>
-            ) : null
-          )}
-        {allowedGeoLocation && (
-          <MapLegendItem>
-            <img src={getIconUrl('user')} height={30} alt="User Marker" />
-            <Text isBold>Ty</Text>
-          </MapLegendItem>
-        )}
-      </MapLegend> */}
 
       {selectedMarker && <MapPopup onCloseClick={onMapClick} selectedMarker={selectedMarker} />}
     </Wrapper>

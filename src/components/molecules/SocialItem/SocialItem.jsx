8b4bb9ebf66de4ef7c3397ba3aa8e8ed3535955a
Dawid Grabek <grabekd37@gmail.com>
2023-12-13 14:17:20 +0100

    feat(groups): enable navigate to group page on click

--- src/components/molecules/SocialItem/SocialItem.jsx
+++ src/components/molecules/SocialItem/SocialItem.jsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router'
+
 import Dot from '@/components/atoms/Dot/Dot'
 import { Text } from '@/components/atoms/Text/Text.styles'
 import { Title } from '@/components/atoms/Title/Title.styles'
@@ -6,9 +8,14 @@ import { Wrapper } from './SocialItem.styles'
 
 export const SocialItem = (props) => {
   const { name, activity, isWithoutTitle, average } = props
+  const navigate = useNavigate()
+
+  const navigateToGroupPage = () => {
+    navigate(`/groups/${props.id}`)
+  }
 
   return (
-    <Wrapper>
+    <Wrapper onClick={navigateToGroupPage}>
       {isWithoutTitle ? null : <Title isBold>{name}</Title>}
       <div>
         <Text isBig>{activity?.name}</Text>

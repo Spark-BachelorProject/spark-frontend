1854537742209d1db60979304266e97f579018c5
Karol Gralewski <gralewski.karol@gmail.com>
2023-12-15 16:50:03 +0100

    feat(profile): select favourite activities

---
+++ src/components/molecules/FavouriteActivitiesSelect/FavouriteActivitiesSelect.jsx
@@ -0,0 +1,71 @@
+import { useEffect, useState } from 'react'
+
+import { Button } from '@/components/atoms/Buttons/Button.styles'
+import { SecondaryButton } from '@/components/atoms/Buttons/SecondaryButton.styles'
+import { Title } from '@/components/atoms/Title/Title.styles'
+import { useGetActivitiesQuery } from '@/store/api/activities'
+
+import {
+  ActivitiesWrapper,
+  StyledBlueText,
+  StyledButton,
+  StyledText,
+  Wrapper,
+} from './FavouriteActivitiesSelect.styles'
+
+export const FavouriteActivitiesSelect = ({ handleClose }) => {
+  const [activities, setActivities] = useState([])
+  const [showAll, setShowAll] = useState(false)
+
+  const { data: activitiesApi, isLoading: isLoadingActivitiesApi } = useGetActivitiesQuery()
+
+  const [selectedActivities, setSelectedActivities] = useState([])
+
+  useEffect(() => {
+    if (!isLoadingActivitiesApi) {
+      if (activitiesApi.length === 0) return
+      const activitiesApiWithValue = activitiesApi.map((activity) => ({
+        ...activity,
+        value: activity.name,
+      }))
+      setActivities(activitiesApiWithValue)
+    }
+  }, [activitiesApi, isLoadingActivitiesApi])
+
+  const handleActivityClick = (activity) => {
+    if (selectedActivities.includes(activity)) {
+      setSelectedActivities(selectedActivities.filter((a) => a !== activity))
+    } else {
+      setSelectedActivities([...selectedActivities, activity])
+    }
+  }
+
+  const displayedActivities = showAll ? activities : activities.slice(0, 15)
+
+  return (
+    <Wrapper>
+      <Title isBig isBold>
+        Wybierz swoje ulubione aktywności
+      </Title>
+      <StyledText>Zaznacz 3 lub więcej</StyledText>
+      <ActivitiesWrapper>
+        {displayedActivities.map((activity, index) => (
+          <Button
+            key={index}
+            isGray={!selectedActivities.includes(activity)}
+            onClick={() => handleActivityClick(activity)}
+          >
+            {activity.name}
+          </Button>
+        ))}
+      </ActivitiesWrapper>
+      <StyledBlueText onClick={() => setShowAll(!showAll)}>
+        {showAll ? 'Pokaż mniej' : 'Pokaż więcej'}
+      </StyledBlueText>
+      <StyledButton isGray onClick={handleClose}>
+        Anuluj
+      </StyledButton>
+      <SecondaryButton>Potwierdź</SecondaryButton>
+    </Wrapper>
+  )
+}
---
+++ src/components/molecules/FavouriteActivitiesSelect/FavouriteActivitiesSelect.styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+
+import { Button } from '@/components/atoms/Buttons/Button.styles'
+import { Text } from '@/components/atoms/Text/Text.styles'
+
+export const Wrapper = styled.div`
+  padding: 0px 20px;
+  width: 600px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`
+
+export const StyledButton = styled(Button)`
+  margin-bottom: -25px;
+`
+
+export const StyledText = styled(Text)`
+  margin-top: -10px;
+`
+
+export const ActivitiesWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  width: 100%;
+  margin-bottom: 15px;
+
+  & > * {
+    margin: -15px 0px;
+  }
+`
+
+export const StyledBlueText = styled(Text)`
+  color: ${({ theme }) => theme.colors.accent};
+  cursor: pointer;
+`
